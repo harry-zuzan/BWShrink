b4113a2b747f11f0eb2b3eea547f78239579f772
Harry Zuzan <iliketobicycle@gmail.com>
2016-05-21 09:41:02 -0700

    removing redescending likelihood that can now be imported from redescendl

--- bws/__init__.py
+++ bws/__init__.py
@@ -4,8 +4,7 @@ from mrf_wavelet_icm import shrink_mrf2 as bws2d
 
 from mrf_wavelet_icm import shrink_mrf1_icm as bws1d
 
-from mrf_wavelet_icm import redescend_residuals_logistic
-from mrf_wavelet_icm import get_redescend_weights_logistic
-
 
 __all__ = ["bws1d","bws2d","bws2d_redescend"]
+
+del mrf_wavelet_icm
