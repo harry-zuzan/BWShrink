40dcdf10b6f55b5e6d498ee0d6f793f79fdd0492
Harry Zuzan <iliketobicycle@gmail.com>
2017-03-08 22:21:09 -0800

    making sure the commit was made after revisions

--- bws/__init__.py
+++ bws/__init__.py
@@ -1,18 +1,18 @@
 
 from mrf_wavelet_icm import shrink_mrf2_redescend as bws2d_redescend
+from mrf_wavelet_icm import shrink_mrf3_redescend as bws3d_redescend
+
 from mrf_wavelet_icm import shrink_mrf2 as bws2d
 from mrf_wavelet_icm import shrink_mrf3 as bws3d
 
 from mrf_wavelet_icm import shrink_mrf1_redescend as bws1d_redescend
 from mrf_wavelet_icm import shrink_mrf1 as bws1d
 from mrf_wavelet_icm import shrink_mrf1_stagger as bws1d_stagger
-from mrf_wavelet_icm import shrink_mrf1_redescend
+
 import test1D
-#from mrf_wavelet_icm import shrink_mrf1_icm as bws1d
-
-from wavelet_trans_3d import wavedec3
 
 
-__all__ = ["bws1d","bws1d_stagger","bws2d","bws2d_redescend","test1D"]
+__all__ = ["bws1d", "bws1d_stagger", "bws2d", "bws2d_redescend",
+				"bws3d", "bws3d_redescend", "test1D"]
 
 del mrf_wavelet_icm
